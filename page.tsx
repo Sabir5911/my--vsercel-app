7d82ee7579c5cf2151d9daad285199b8eddf5996
Sabir5911 <115262495+Sabir5911@users.noreply.github.com>
2023-01-02 18:41:49 +0500

    initial

---
+++ page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from './page.module.css'
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+          <div>  Hello from Nawz sharif ma abhi nahi a raha bd mka ao ga : BY </div>
+
+  )
+}
+
